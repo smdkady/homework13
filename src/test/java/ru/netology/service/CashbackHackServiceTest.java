4c744d9b83a7670d0000efe7ae733640b35ab1b6
vexed <kadirthekid@gmail.com>
2024-04-03 17:21:49 +0300

    changed test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+
+    @org.testng.annotations.Test
+    public void testWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+@org.testng.annotations.Test
+    public void testUnderCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+}
+
+}
